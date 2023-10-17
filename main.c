56bf17e5cc81a2aa57d0f64979b7054fb7728f31
abdulmomen96 <abdu.galga@gmail.com>
2023-10-17 22:11:02 +0300

    add the text splitting function

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 #include <string.h>
 
 int main(){
-    
+    char text[50] = "Hello World! Hey\n";
+    char *splitted_text = strtok(text, " ");
+
+    while (splitted_text != NULL)
+    {
+        printf("%s\n", splitted_text);
+        splitted_text = strtok(NULL, " ");
+    }
+    return 0;
 }
